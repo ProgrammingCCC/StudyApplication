179c0fc9cffb265014775b99e208c0a7e4ecd084
Janell Baxter <jbaxter@colum.edu>
2017-01-30 11:10:25 -0600

    Added Term class

---
+++ StudyApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StudyApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Study Application");
+        }
+    }
+    class Term
+    {
+        string Word;
+        string Defintion;
+    }
+}
